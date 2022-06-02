6190c7e6d40f4d26c5422b1313aa6f6683b2e27d
Arun Kant Sharma <arunkant.sharma@qure.ai>
2022-06-02 20:01:43 +0530

    clean slate for creating bindings

--- fluent-bit-js.cc
+++ fluent-bit-js.cc
@@ -1,76 +1,10 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-
-/*  Fluent Bit Demo
- *  ===============
- *  Copyright (C) 2015 Treasure Data Inc.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
+// Not using any header file. Can refactor when needed
 #include <fluent-bit.h>
 #include <napi.h>
 
-
-int example_function()
-{
-    int i;
-    int n;
-    char tmp[256];
-    flb_ctx_t *ctx;
-    int in_ffd;
-    int out_ffd;
-
-    /* Initialize library */
-    ctx = flb_create();
-
-    in_ffd = flb_input(ctx, (char *) "lib", NULL);
-    flb_input_set(ctx, in_ffd, (char *) "test", NULL);
-
-    out_ffd = flb_output(ctx, (char *) "stdout", NULL);
-    flb_output_set(ctx, out_ffd, (char *) "test", NULL);
-
-    /* Start the background worker */
-    flb_start(ctx);
-
-    /* Push some data */
-    for (i = 0; i < 100; i++) {
-        n = snprintf(tmp, sizeof(tmp) - 1,
-                     "[%lu, {\"key\": \"val %i\"}]",
-                     time(NULL), i);
-        printf("%s\n", tmp);
-        flb_lib_push(ctx, in_ffd, tmp, n);
-    }
-
-    flb_stop(ctx);
-
-    /* Release Resources */
-    flb_destroy(ctx);
-
-    return 0;
-}
-
-Napi::Number example_function_wrapped(const Napi::CallbackInfo& info){
- Napi::Env env = info.Env(); //check if arguments are integer only.
- 
- Napi::Number returnValue = Napi::Number::New(env, example_function());
- 
- return returnValue;
-}
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
-  //export Napi function
-  exports.Set(Napi::String::New(env, "example_function"), Napi::Function::New(env, example_function_wrapped));
   return exports;
 }
 
-NODE_API_MODULE(example_function_wrapped, Init)
+NODE_API_MODULE(fluentbit, Init)
--- fluent-bit-js.h
+++
@@ -1,9 +0,0 @@
-#include <napi.h>
-#include <fluent-bit.h>
-using namespace std;
-
-//add number function
-int example_function();
-Napi::Number example_function_wrapped(const Napi::CallbackInfo& info); //Export API
-Napi::Object Init(Napi::Env env, Napi::Object exports);
-NODE_API_MODULE(addon, Init)
